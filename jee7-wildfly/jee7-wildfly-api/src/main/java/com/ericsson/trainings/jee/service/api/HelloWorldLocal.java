fefc9c5f644e31590db37a25d21156faedb6b83d
dekapx <xkapil@gmail.com>
2015-07-07 23:38:32 +0100

    jee-7, wildfly project added

---
+++ jee7-wildfly/jee7-wildfly-api/src/main/java/com/ericsson/trainings/jee/service/api/HelloWorldLocal.java
@@ -0,0 +1,5 @@
+package com.ericsson.trainings.jee.service.api;
+
+public interface HelloWorldLocal {
+	String sayHello(String arg);
+}
---
+++ jee7-wildfly/jee7-wildfly-api/src/main/java/com/ericsson/trainings/jee/service/impl/HelloWorldBean.java
@@ -0,0 +1,13 @@
+package com.ericsson.trainings.jee.service.impl;
+
+import com.ericsson.trainings.jee.service.api.HelloWorldLocal;
+
+public class HelloWorldBean implements HelloWorldLocal {
+
+	@Override
+	public String sayHello(String arg) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
