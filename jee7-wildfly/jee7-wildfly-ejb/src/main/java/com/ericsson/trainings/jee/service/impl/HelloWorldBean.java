8a4e4f3c30f0c82445512d20cdceda0432033307
ekalkur <kapil.k.kumar@ericsson.com>
2015-07-16 12:19:18 +0100

    jse client added for remote lookup

--- jee7-wildfly/jee7-wildfly-ejb/src/main/java/com/ericsson/trainings/jee/service/impl/HelloWorldBean.java
+++ jee7-wildfly/jee7-wildfly-ejb/src/main/java/com/ericsson/trainings/jee/service/impl/HelloWorldBean.java
@@ -7,6 +7,7 @@ import javax.annotation.PreDestroy;
 import javax.ejb.AsyncResult;
 import javax.ejb.Asynchronous;
 import javax.ejb.Local;
+import javax.ejb.Remote;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -14,12 +15,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ericsson.trainings.jee.service.api.HelloWorldLocal;
+import com.ericsson.trainings.jee.service.api.HelloWorldRemote;
 import com.ericsson.trainings.jee.service.util.TextMessageFormatter;
 
 @Stateless
 @Local(HelloWorldLocal.class)
-public class HelloWorldBean implements HelloWorldLocal {
-	
+@Remote(HelloWorldRemote.class)
+public class HelloWorldBean implements HelloWorldLocal, HelloWorldRemote {
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldBean.class);
 
 	@Inject
@@ -55,5 +58,5 @@ public class HelloWorldBean implements HelloWorldLocal {
 		LOGGER.info("sayHelloAsync() method invoked with parameter \"{}\"", arg);
 		return new AsyncResult<String>("Hello !!! " + arg);
 	}
-	
+
 }
