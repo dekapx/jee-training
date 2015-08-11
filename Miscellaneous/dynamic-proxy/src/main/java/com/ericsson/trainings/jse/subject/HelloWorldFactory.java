d104374b9c66855e81c991465648bfaff7977166
ekalkur <kapil.k.kumar@ericsson.com>
2015-08-11 15:01:50 +0100

    code rwcactoring

--- Miscellaneous/dynamic-proxy/src/main/java/com/ericsson/trainings/jse/subject/HelloWorldFactory.java
+++ Miscellaneous/dynamic-proxy/src/main/java/com/ericsson/trainings/jse/subject/HelloWorldFactory.java
@@ -3,7 +3,7 @@ package com.ericsson.trainings.jse.subject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.ericsson.trainings.jse.proxy.HelloWorldProxy;
+import com.ericsson.trainings.jse.proxy.DynamicProxy;
 
 public enum HelloWorldFactory {
 	INSTANCE;
@@ -12,6 +12,6 @@ public enum HelloWorldFactory {
 
 	public HelloWorld getHelloWorldProxy() {
 		LOGGER.info("Create HelloWorld proxy object...");
-		return (HelloWorld) HelloWorldProxy.newInstance(new HelloWorldImpl());
+		return (HelloWorld) DynamicProxy.newInstance(new HelloWorldImpl());
 	}
 }
