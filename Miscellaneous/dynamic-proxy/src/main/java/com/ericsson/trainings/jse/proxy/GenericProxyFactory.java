a714103b5b5a90d1b829293195a4f6d6c183d4db
ekalkur <kapil.k.kumar@ericsson.com>
2015-08-11 16:27:22 +0100

    generic factory implemented

--- Miscellaneous/dynamic-proxy/src/main/java/com/ericsson/trainings/jse/proxy/GenericProxyFactory.java
+++ Miscellaneous/dynamic-proxy/src/main/java/com/ericsson/trainings/jse/proxy/GenericProxyFactory.java
@@ -1,5 +1,34 @@
 package com.ericsson.trainings.jse.proxy;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+import com.ericsson.trainings.jse.subject.HelloWorld;
+import com.ericsson.trainings.jse.subject.HelloWorldImpl;
+
 public enum GenericProxyFactory {
 	INSTANCE;
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public <T> T getObjectByClassType(final Class<T> clazz) {
+		final Constructor constructor = clazz.getConstructors()[0];
+		try {
+			return (T) DynamicProxy.newInstance(constructor.newInstance());
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+		final GenericProxyFactory factory = GenericProxyFactory.INSTANCE;
+		HelloWorld helloWorld = factory.getObjectByClassType(HelloWorldImpl.class);
+		helloWorld.sayHello("test");
+	}
 }
