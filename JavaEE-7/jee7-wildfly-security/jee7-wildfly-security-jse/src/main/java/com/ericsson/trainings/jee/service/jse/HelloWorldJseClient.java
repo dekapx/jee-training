607fc0f4be9f14373c107973fc3ba4475e616014
dekapx <xkapil@gmail.com>
2015-08-17 22:06:29 +0100

    security specific config added

--- JavaEE-7/jee7-wildfly-security/jee7-wildfly-security-jse/src/main/java/com/ericsson/trainings/jee/service/jse/HelloWorldJseClient.java
+++ JavaEE-7/jee7-wildfly-security/jee7-wildfly-security-jse/src/main/java/com/ericsson/trainings/jee/service/jse/HelloWorldJseClient.java
@@ -1,5 +1,6 @@
 package com.ericsson.trainings.jee.service.jse;
 
+import java.io.File;
 import java.util.Properties;
 import java.util.concurrent.Future;
 
@@ -29,6 +30,15 @@ public class HelloWorldJseClient {
 		final Properties jndiProps = new Properties();
 		jndiProps.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
 		jndiProps.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
+		final String PKG_INTERFACES = "org.jboss.ejb.client.naming";
+		jndiProps.put(Context.URL_PKG_PREFIXES, PKG_INTERFACES);
+		jndiProps.put("jboss.naming.client.connect.options.org.xnio.Options.SASL_POLICY_NOPLAINTEXT", "false");
+
+		jndiProps.put("jboss.naming.client.remote.connectionprovider.create.options.org.xnio.Options.SSL_ENABLED", "true");
+		jndiProps.put("jboss.naming.client.connect.options.org.xnio.Options.SSL_STARTTLS", "true");
+
+		defineSystemProperties();
+
 		final Context context = new InitialContext(jndiProps);
 
 		final HelloWorldRemote remote = (HelloWorldRemote) context.lookup(createRemoteJndiName());
@@ -40,5 +50,15 @@ public class HelloWorldJseClient {
 		final String appName = "jee7-wildfly-security-war";
 		final String beanName = "HelloWorldBean";
 		return appName + "/" + beanName + "!" + HelloWorldRemote.class.getName();
+	}
+
+	public static void defineSystemProperties() {
+		final File clientKeyStore = new File("src/main/resources/client-keystore.jks");
+		System.setProperty("javax.net.ssl.keyStore", clientKeyStore.getAbsolutePath());
+		System.setProperty("javax.net.ssl.keyStorePassword", "changeit");
+
+		final File clientTrustStore = new File("src/main/resources/client-truststore.jks");
+		System.setProperty("javax.net.ssl.trustStore", clientTrustStore.getAbsolutePath());
+		System.setProperty("javax.net.ssl.trustStorePassword", "changeit");
 	}
 }
