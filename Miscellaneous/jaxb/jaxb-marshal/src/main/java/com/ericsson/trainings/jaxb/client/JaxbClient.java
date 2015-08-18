c3b07e5055bbb7bb0b0679d4f17cc14d39dd3834
ekalkur <kapil.k.kumar@ericsson.com>
2015-08-18 17:32:53 +0100

    code refactoring

---
+++ Miscellaneous/jaxb/jaxb-marshal/src/main/java/com/ericsson/trainings/jaxb/client/JaxbClient.java
@@ -0,0 +1,30 @@
+package com.ericsson.trainings.jaxb.client;
+
+import java.io.File;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import com.ericsson.trainings.jaxb.beans.Student;
+
+public class JaxbClient {
+	public static void main(String[] args) {
+		final Student student = new Student();
+		student.setId(1L);
+		student.setFirstName("De");
+		student.setLastName("Kapx");
+		student.setMarks(87.69);
+
+		final File file = new File("src/main/resources/xml/student.xml");
+		try {
+			final JAXBContext jaxbContext = JAXBContext.newInstance(Student.class);
+			final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			jaxbMarshaller.marshal(student, file);
+			jaxbMarshaller.marshal(student, System.out);
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- Miscellaneous/jaxb/jaxb-marshal/src/main/java/com/ericsson/trainings/jaxb/marshaller/JaxbMarshaller.java
+++ Miscellaneous/jaxb/jaxb-marshal/src/main/java/com/ericsson/trainings/jaxb/marshaller/JaxbMarshaller.java
@@ -1,30 +1,7 @@
 package com.ericsson.trainings.jaxb.marshaller;
 
-import java.io.File;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-
-import com.ericsson.trainings.jaxb.beans.Student;
-
 public class JaxbMarshaller {
-	public static void main(String[] args) {
-		final Student student = new Student();
-		student.setId(1L);
-		student.setFirstName("De");
-		student.setLastName("Kapx");
-		student.setMarks(87.69);
+	public void marshall() {
 
-		final File file = new File("src/main/resources/xml/student.xml");
-		try {
-			final JAXBContext jaxbContext = JAXBContext.newInstance(Student.class);
-			final Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-			jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-			jaxbMarshaller.marshal(student, file);
-			jaxbMarshaller.marshal(student, System.out);
-		} catch (JAXBException e) {
-			e.printStackTrace();
-		}
 	}
 }
