a5f21ed5e942c8ba3b4564ce84ad8da1742cb438
ekalkur <kapil.k.kumar@ericsson.com>
2015-08-21 16:06:28 +0100

    code cleanup

--- JavaEE-7/jee7-wildfly-security/jee7-wildfly-security-api/src/main/java/com/ericsson/trainings/jee/service/api/HelloWorldRemote.java
+++
@@ -1,4 +0,0 @@
-package com.ericsson.trainings.jee.service.api;
-
-public interface HelloWorldRemote extends HelloWorldLocal {
-}
--- Miscellaneous/java-concurrency/src/main/java/com/ericsson/trainings/jse/concurrency/runnable/RunnableDemo.java
+++
@@ -1,8 +0,0 @@
-package com.ericsson.trainings.jse.concurrency.runnable;
-
-public class RunnableDemo {
-	public static void main(String[] args) {
-		Runnable taskRunner = new TaskRunner();
-		new Thread(taskRunner, "TaskRunner").start();
-	}
-}
